4dc531e28e159f01bda3660d8664645da8d757c2
Asante <asanterashid.biz@gmail.com>
2021-05-05 15:30:51 -0400

    added routes in app.js using switch routes. will need to change to hashrouter later

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # React-Portfolio
 Personal portfolio built using react components 
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+* Deploy this site to GitHub Pages using the [Create React App docs for deployment.](https://create-react-app.dev/docs/deployment/#github-pages)
+
+* **Important**: If you use React Router, you will **NOT** be able to use the `<BrowserRouter>` component on GitHub Pages. Read the [GitHub note on client-side routing](https://create-react-app.dev/docs/deployment/#notes-on-client-side-routing) and the [React Router documentation on HashRouter](https://reactrouter.com/web/api/HashRouter) for more information.
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import logo from './logo.svg';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import pages and nav tab liek W19_A22
-import './App.css';
+import Nav from "./components/header";
+import About from "./components/pages/about";
+import Resume from "./components/pages/resume";
+import Gallery from "./components/pages/gallery";
+import Contact from "./components/footer";
+// import './App.css';
+//? may ned to tyake out swith case in order to render the entire page at ionve instead of breaking it apart 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/home" component={About} />
+        <Route exact path="/favorites" component={Resume} />
+        <Route exact path="/posts/:id" component={Gallery} />
+        <Route component={NoMatch} />
+      </Switch>
+      <Contact />
     </div>
+  </Router>
   );
 }
 
